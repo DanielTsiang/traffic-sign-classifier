182aa9f76d6d2b7f5a712679e50670598036e19a
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2022-01-25 00:19:41 +0000

    Simplify gunicorn_config.py

--- services/flask/gunicorn_config.py
+++ services/flask/gunicorn_config.py
@@ -4,15 +4,6 @@ from multiprocessing import cpu_count
 
 warnings.filterwarnings("ignore")
 
-
-def int_env(env_var: str, default: int) -> int:
-    """
-    Cast environment variable to int
-    :return:
-    """
-    return int(env(env_var, default))
-
-
 # App level
 # The port to bind.
 port = int(env("GUNICORN_PORT", "8000"))
@@ -20,11 +11,11 @@ port = int(env("GUNICORN_PORT", "8000"))
 # The socket to bind.
 bind = env("GUNICORN_BIND", f"0.0.0.0:{port}")
 
-# The number of worker processes that this serve
-workers = int_env("GUNICORN_WORKERS", (2 * cpu_count()) + 1)
+# The number of worker processes that this serves
+workers = int(env("GUNICORN_WORKERS", (2 * cpu_count()) + 1))
 
 worker_tmp_dir = env("GUNICORN_WORKER_TMP_DIR", "/tmp")
 
-# If a worker does not notify the master process in this number of
-# seconds it is killed and a new worker is spawned to replace it.
-timeout = int_env("GUNICORN_TIMEOUT", 30)
+# If a worker does not notify the master process in this number of seconds,
+# it is killed and a new worker is spawned to replace it.
+timeout = int(env("GUNICORN_TIMEOUT", 30))
