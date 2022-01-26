0d644544c13cfc9e9d8887c1c3a884e24b20a9a8
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2022-01-26 12:14:16 +0000

    Make COPY paths more clear in Dockerfiles

--- services/tensorflow-serving/Dockerfile.tf
+++ services/tensorflow-serving/Dockerfile.tf
@@ -4,8 +4,8 @@ ENV MODEL_BASE_PATH /models
 ENV MODEL_NAME traffic-sign-classifier
 ENV PORT 8501
 
-COPY ./model/ /models/traffic-sign-classifier
-COPY ./model.config /models/
+COPY ./model/ /models/traffic-sign-classifier/
+COPY ./model.config /models/model.config
 
 # Fix because base tf_serving_entrypoint.sh does not take $PORT env variable while $PORT is set by Heroku
 # CMD is required to run on Heroku
