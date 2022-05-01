209f5d55f193e186e822f1f7e6f65ec61108c252
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2022-05-01 00:41:14 +0000

    Use docker-compose to run integration tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,13 @@
 # Run integration tests
-echo "Starting Docker containers..."
+echo "======= Starting Docker containers ======="
 docker-compose up -d --build
 
-echo "Running integration tests..."
-python3 -m unittest discover -s tests  -p "test*"
+echo "======= Running integration tests ======="
+docker-compose run --rm \
+  -e TF_CPP_MIN_LOG_LEVEL="3" \
+  -v "$(pwd)/tests:/tests" \
+  -v "$(pwd)/services:/services" \
+  flask python3 -m unittest discover -s /tests -p "test*"
 
-echo "Cleaning up Docker..."
+echo "======= Cleaning up Docker ======="
 docker-compose down
