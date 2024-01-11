7e397b02b5b035636ed5871716673bf98380cc49
Sourcery AI <>
2024-01-11 00:32:07 +0000

    'Refactored by Sourcery'

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 root_path = Path(__file__).parents[1].resolve()
 source_code_path = root_path
-sys.path.extend([root_path.as_posix(), source_code_path.as_posix()])
+sys.path.extend([source_code_path.as_posix(), source_code_path.as_posix()])
 SAMPLE_PATH = source_code_path / "static" / "sample"
